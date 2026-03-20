c4897885661eec03d9945c7c0d8514c4db0334a4
harrydoungel <harrydoungel@gmail.com>
2026-03-21 05:13:57 +0530

    switch back to env firebase

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,7 +1,6 @@
 const admin = require("firebase-admin");
 
-// ✅ Use JSON file directly (correct method)
-const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 if (!admin.apps.length) {
   admin.initializeApp({
