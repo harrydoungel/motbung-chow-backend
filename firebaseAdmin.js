a9da2c4583243796750b793f2269e3ab001ccc97
harrydoungel <harrydoungel@gmail.com>
2026-04-05 21:16:51 +0530

    fix firebase path

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = require("../firebase-service-account.json");
 
 if (!admin.apps.length) {
   admin.initializeApp({
