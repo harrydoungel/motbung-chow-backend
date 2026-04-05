53152fa948e2cb03759513b5ce99f8b0c34c2a2b
harrydoungel <harrydoungel@gmail.com>
2026-04-05 21:28:10 +0530

    use env firebase key

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,6 +1,8 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../firebase-service-account.json");
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_SERVICE_ACCOUNT
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
